70ece78d8a203ca57cd7150d04e51df307cd23ee
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-12-15 14:24:49 -0800

    0.6.8

--- showtime/__init__.py
+++ showtime/__init__.py
@@ -4,7 +4,7 @@
 Circus tent emoji state tracking for Apache Superset ephemeral environments.
 """
 
-__version__ = "0.6.7"
+__version__ = "0.6.8"
 __author__ = "Maxime Beauchemin"
 __email__ = "maximebeauchemin@gmail.com"
 
